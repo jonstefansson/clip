c6e9495a3d57ea210d95dcc78973998b7a8b22b9
Jon Stefansson <jon.stefansson@thereformation.us>
2024-08-23 10:44:59 -0700

    Modifies font
    * increase size
    * set family to a monospaced family

--- src/clip/ui/clipboard.py
+++ src/clip/ui/clipboard.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QVBoxLayout, QScrollArea
-from PySide6.QtGui import QClipboard
+from PySide6.QtGui import QClipboard, QFont
 
 
 class ClipboardApp(QWidget):
@@ -14,14 +14,19 @@ class ClipboardApp(QWidget):
         scroll_area = QScrollArea()
         scroll_widget = QWidget()
         scroll_layout = QGridLayout()
+        font = QFont()
+        font.setFamily('Andale Mono')
+        font.setPointSize(18)
 
         # Create buttons for each pair
         for i, (label, value) in enumerate(self.pairs):
             row = i
             button = QPushButton(label, self)
+            button.setFont(font)
             button.clicked.connect(lambda checked, val=label: self.copy_to_clipboard(val))
             scroll_layout.addWidget(button, row, 0)
             button = QPushButton(value, self)
+            button.setFont(font)
             button.clicked.connect(lambda checked, val=value: self.copy_to_clipboard(val))
             scroll_layout.addWidget(button, row, 1)
 
